bbe2f34c01629866549ca2a24a4aa4cf22d0797d
u1375369 <steven.blanc521@gmail.com>
2026-06-03 16:50:49 -0600

    added mysql database

--- src/backend/WebServer/Program.cs
+++ src/backend/WebServer/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
 using WebServer;
 
 // create the builder
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 33))
+    )
+);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
